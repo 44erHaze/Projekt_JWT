dc677e675b69358d31d2621f3899f7c80fd9865c
44erHaze <anthony.franz7@gmail.com>
2023-11-21 18:41:36 +0100

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using JWTAuthentication.Models;
+using JWTAuthentication.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace JWTAuthentication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+        public AccountController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] AuthenticateUser model)
+        {
+            if (model.Username != "admin" || model.Password != "1234")
+                return Unauthorized("Invalid Credentials");
+            else
+                return new JsonResult(new { userName = model.Username, token = _tokenService.CreateToken(model.Username) });
+        }
+    }
+}
---
+++ Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthentication.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GETController : Controller
+    {
+        [HttpGet("Mitarbeiter")]
+        [AllowAnonymous]
+        public string First()
+        {
+            return "Susanne Hilfiger \n" +
+                "Friedrich-strasse 69 \n" +
+                "079 420 69 69 \n" +
+                "Grosser Service \n" +
+                "Standard";
+        }
+
+        [HttpGet("Admin")]
+        [Authorize]
+        public string Second()
+        {
+            return "Tony Montana \n" +
+                "Königsalee 69 \n" +
+                "061 420 44 44 \n" +
+                "Heisswaschen \n" +
+                "Express";
+        }
+    }
+}
